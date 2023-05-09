6c94896267aafaf1433a13a0d2e7a6a72f0dbbfe
Bernhard Heinloth <heinloth@cs.fau.de>
2023-05-09 22:53:42 +0200

    update update script

--- update.sh
+++ update.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+# Change to script directory
+cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null
 
 # Fetch latest version
 git pull --ff-only origin master
 
 # Run setup (again)
-"$DIR/setup.sh"
+./setup.sh
 
 # Continue with program from parameter (hacky)
 if [[ $# -gt 0 ]] ; then
